e43433716257446c1029b19254e60dbd5df602cf
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2018-10-15 12:02:20 +0100

    Renamed parameter

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,35 +1,35 @@
 const STATUS_CREATED = 201
 const STATUS_OK = 200
-var notificationNumbers = {}
+var notifications = {}
 
 const handlers = {
   get: (request, h) => {
-    return notificationNumbers[request.params.notificationNumber]
+    return notifications[request.params.id]
   },
   put: (request, h) => {
     let responseCode
     // Return the correct response code for PUT requests based on whether or not the notic
-    if (notificationNumbers[request.params.notificationNumber]) {
+    if (notifications[request.params.id]) {
       responseCode = STATUS_OK
     } else {
       responseCode = STATUS_CREATED
     }
     // As the initial implementation is just a fake, use simple in memory storage.
-    notificationNumbers[request.params.notificationNumber] = request.payload
+    notifications[request.params.id] = request.payload
     return h.response().code(responseCode)
   }
 }
 
 module.exports = [{
   method: 'GET',
-  path: '/notification/{notificationNumber}',
+  path: '/notification/{id}',
   options: {
     handler: handlers.get
   }
 },
 {
   method: 'PUT',
-  path: '/notification/{notificationNumber}',
+  path: '/notification/{id}',
   options: {
     handler: handlers.put
   }
