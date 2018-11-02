0587b1d9385a9d1a192b4fa6d798ded02b4a022b
John Watson <john.watson2@rpa.gov.uk>
2018-11-02 14:41:57 +0000

    initial sequelize configured

--- server/config.js
+++ server/config.js
@@ -4,12 +4,24 @@ const joi = require('joi')
 const schema = {
   port: joi.number().default(3001),
   env: joi.string().valid('development', 'test', 'production').default('development')
+  // postgresUser: joi.string().default('postgres'),
+  // postgresPassword: joi.string.default('postgres'),
+  // postgresDatabase: joi.string().required(),
+  // postgresHost: joi.string().default('localhost'),
+  // postgresPort: joi.number().default(5432),
+  // dialect: joi.string().default('postgres')
 }
 
 // Build config
 const config = {
   port: process.env.PORT,
   env: process.env.NODE_ENV
+  // postgresUser: process.env.POSTGRES_USERNAME,
+  // postgresPassword: process.env.IWS_POSTGRES_PASSWORD,
+  // postgresDatabase: 'iws',
+  // postgresHost: '127.0.0.1',
+  // postgresPort: 5432,
+  // dialect: 'postgres'
 }
 
 // Validate config
