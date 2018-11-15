50f4ac5c8da1714ce3560be050b158103dcb1b01
John Watson <john.watson2@rpa.gov.uk>
2018-11-15 11:55:12 +0000

    notification service refactored

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,7 +1,5 @@
 // **An initial in memory fake implementation - API versioning approach to be discussed**
 const notificationService = require('../services/notification-service')
-const STATUS_BAD_REQUEST = 400
-const STATUS_CREATED = 201
 const STATUS_OK = 200
 
 const handlers = {
@@ -9,26 +7,12 @@ const handlers = {
     return notificationService.get(request.params.id)
   },
   post: async (request, h) => {
-    let responseCode
-    if (await notificationService.getbynumber(request.payload.notificationNumber) != null) {
-      // There does not appear to be a standard for responding to duplicate POSTs.
-      // Return a HTTP 400 status code for now.
-      responseCode = STATUS_BAD_REQUEST
-    } else {
-      notificationService.create(request)
-      responseCode = STATUS_CREATED
-    }
-    return h.response().code(responseCode)
+    await notificationService.createOrUpdate(request)
+    return h.response().code(STATUS_OK)
   },
   put: async (request, h) => {
-    let responseCode
-    // Return the correct response code for PUT requests based on whether or not the notification has been 'created'.
-    if (await notificationService.getbynumber(request.payload.notificationNumber)) {
-      responseCode = STATUS_OK
-    } else {
-      responseCode = STATUS_CREATED
-    }
-    return h.response().code(responseCode)
+    await notificationService.createOrUpdate(request)
+    return h.response().code(STATUS_OK)
   }
 }
 
--- server/services/notification-service.js
+++ server/services/notification-service.js
@@ -3,28 +3,30 @@ const Op = require('sequelize').Op
 const uuid = require('uuid')
 
 module.exports = {
-  create: async function (request) {
-    const id = uuid.v4()
-    request.payload.id = id
-
-    let competentAuthority = await db.lookup_competentauthority.findOne({ where: { abbreviation: request.payload.authority.toUpperCase() } })
-    let unitedKingdomCompetentAuthority = await db.lookup_unitedkingdomcompetentauthority.findOne({ where: { competentauthorityid: competentAuthority.id } })
-    let shipmentType = await db.lookup_notificationtype.findOne({ where: { description: { [Op.iLike]: request.payload.type } } })
-    let notification = {
-      id: id,
-      notificationtype: shipmentType.id,
-      notificationnumber: request.payload.notificationNumber,
-      userid: 'user',
-      rowversion: '1',
-      competentauthority: unitedKingdomCompetentAuthority.id,
-      createddate: new Date(),
-      reasonforexport: 'reason',
-      hasspecialhandlingrequirements: true,
-      specialhandlingdetails: 'details',
-      isrecoverypercentagedataprovidedbyimporter: true,
-      wastegenerationprocess: 'process',
-      iswastegenerationprocessattached: true
+  createOrUpdate: async function (request) {
+    let notification = await this.getByNumber(request.payload.notificationNumber)
+    // if no notification exists then create a new one with default values
+    if (notification == null) {
+      notification = {
+        id: this.generateId(),
+        notificationnumber: request.payload.notificationNumber,
+        userid: 'user',
+        rowversion: '1',
+        createddate: new Date(),
+        reasonforexport: 'reason',
+        hasspecialhandlingrequirements: true,
+        specialhandlingdetails: 'details',
+        isrecoverypercentagedataprovidedbyimporter: true,
+        wastegenerationprocess: 'process',
+        iswastegenerationprocessattached: true
+      }
     }
+
+    // update values of data supplied from front end
+    let competentAuthority = await this.getCompetentAuthority(request.payload.authority)
+    notification.competentauthority = competentAuthority.id
+    let shipmentType = await this.getShipmentType(request.payload.type)
+    notification.notificationtype = shipmentType.id
 
     db.notification_notification.upsert(notification)
   },
@@ -32,8 +34,20 @@ module.exports = {
     let notification = await db.notification_notification.findOne({ where: { id: id } })
     return notification
   },
-  getbynumber: async function (notificationNumber) {
+  getByNumber: async function (notificationNumber) {
     let notification = await db.notification_notification.findOne({ where: { notificationnumber: notificationNumber } })
     return notification
+  },
+  generateId: function () {
+    return uuid.v4()
+  },
+  getShipmentType: async function (type) {
+    let shipmentType = await db.lookup_notificationtype.findOne({ where: { description: { [Op.iLike]: type } } })
+    return shipmentType
+  },
+  getCompetentAuthority: async function (authority) {
+    let competentAuthority = await db.lookup_competentauthority.findOne({ where: { abbreviation: authority.toUpperCase() } })
+    let unitedKingdomCompetentAuthority = await db.lookup_unitedkingdomcompetentauthority.findOne({ where: { competentauthorityid: competentAuthority.id } })
+    return unitedKingdomCompetentAuthority
   }
 }
