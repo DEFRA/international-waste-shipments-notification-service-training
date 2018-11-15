4e6f2fe33c7841769c9835652527308eab695fd9
John Watson <john.watson2@rpa.gov.uk>
2018-11-15 12:49:47 +0000

    unit tests updated

--- test/index.js
+++ test/index.js
@@ -1,19 +1,43 @@
 const Lab = require('lab')
 const Code = require('code')
+const sinon = require('sinon')
 const lab = exports.lab = Lab.script()
 const createServer = require('../server')
+const notificationService = require('../server/services/notification-service')
 
 lab.experiment('API test', () => {
   let server
+  let sandbox
+  let notifications = []
 
   // Create server before the tests.
   lab.before(async () => {
     server = await createServer()
+    notifications = [{
+      id: '0001',
+      notificationnumber: 'GB001',
+      notificationtype: 1,
+      competentauthority: 1
+    },
+    {
+      id: '0002',
+      notificationnumber: 'GB002',
+      notificationtype: 1,
+      competentauthority: 2
+    }]
   })
 
   // Stop server after the tests.
   lab.after(async () => {
     await server.stop()
+  })
+
+  lab.beforeEach(async () => {
+    sandbox = await sinon.createSandbox()
+  })
+
+  lab.afterEach(async () => {
+    await sandbox.restore()
   })
 
   lab.test('GET / route works', async () => {
@@ -49,12 +73,32 @@ lab.experiment('API test', () => {
   })
 
   lab.test('Retrieval of a non-existent notification returns a HTTP 404 status code', async () => {
+    var mockGet = sinon.stub(notificationService, 'get').callsFake(function fakeGet (id) {
+      return notifications.find(x => x.id === id)
+    })
+
     const options = {
       method: 'GET',
-      url: '/notification/0002'
+      url: '/notification/0003'
     }
 
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(404)
+    mockGet.restore()
+  })
+
+  lab.test('Retrieval of existing notification returns a HTTP 200 status code', async () => {
+    var mockGet = sinon.stub(notificationService, 'get').callsFake(function fakeGet (id) {
+      return notifications.find(x => x.id === id)
+    })
+
+    const options = {
+      method: 'GET',
+      url: '/notification/0001'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    mockGet.restore()
   })
 })
